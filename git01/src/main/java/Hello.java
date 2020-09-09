e9be600c95379b4c2ebe1aca19cbcb98effff90f
lxlxlxlxlxlxlxlx <1209110968@qq.com>
2020-09-09 17:45:26 +0800

    修改一下

--- git01/src/main/java/Hello.java
+++ git01/src/main/java/Hello.java
@@ -8,11 +8,21 @@ public class Hello {
         System.out.println("hello");
         System.out.println("hello123");
         System.out.println("helloword");
+
     }
 
     public static void main(String[] args) {
         System.out.println("helloword!!");
         System.out.println("111");
         System.out.println("222");
+        System.out.println("3333");
+        System.out.println("444");
+        System.out.println("555");
+        System.out.println("666");
+        System.out.println("777");
+
+
+
+
     }
 }
