d8d85f2e2ae4e8d1287a58ef559069e517e3eec8
Administrator <2477692628@qq.com>
2020-09-09 16:33:17 +0800

    创建了一个项目

---
+++ git01/src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @program: git01
+ * @author: wenlong
+ * @create: 2020-09-09 16:26
+ */
+public class Hello {
+    public void hello(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("helloword!!");
+    }
+}
---
+++ git01/src/test/java/com/itheima/git01/Git01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.git01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
