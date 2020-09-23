9165b1642fb9f93d82291ae2bd1ffaf11b61ecaa
Administrator <2477692628@qq.com>
2020-09-23 09:26:22 +0800

    提交一下

---
+++ git03/src/main/java/com/itheima/test/Demo.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+/**
+ * @program: git03
+ * @author: wenlong
+ * @create: 2020-09-23 09:22
+ */
+public class Demo {
+    private static int did;
+
+    public static void main(String[] args) {
+        System.out.println(did=8);
+    }
+}
