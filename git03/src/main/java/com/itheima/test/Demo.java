82d732520260d20540189a2045de57d4a56c6253
Administrator <2477692628@qq.com>
2020-09-23 19:01:07 +0800

    Revert "再次修改一下"
    This reverts commit 2d572bd9188da73e6f421b99d229635ccd940a4a.

--- git03/src/main/java/com/itheima/test/Demo.java
+++ git03/src/main/java/com/itheima/test/Demo.java
@@ -11,5 +11,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println(did=8);
         System.out.println(did=9);
+        System.out.println(did=10);
     }
 }
