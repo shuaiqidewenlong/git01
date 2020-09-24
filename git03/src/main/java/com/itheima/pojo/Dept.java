9b285ea30bbc52073671c842a86bcce99649f337
Administrator <2477692628@qq.com>
2020-09-24 08:40:50 +0800

    laoban 修改了一下

---
+++ git03/src/main/java/com/itheima/pojo/Dept.java
@@ -0,0 +1,15 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * @program: git03
+ * @author: wenlong
+ * @create: 2020-09-24 08:36
+ */
+@Data
+public class Dept {
+    private  int did;
+    private  String dname;
+    private  String dtel;
+}
